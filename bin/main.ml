1be84ca420ab613c796c47269f4db00239ec5b1a
Lucas Rodriguez <lucasrodriguez@free.fr>
2025-02-20 17:15:28 +0100

    JSON extraction for movies

--- bin/main.ml
+++ bin/main.ml
@@ -18,8 +18,12 @@ let universe_tag = function
   | GAMES -> "3"
   | BDS -> "6"
 
+let username = "ryoh"
+
+let json_members = ["props"; "pageProps"; "__APOLLO_STATE__"]
+
 let sc_endpoint page uni =
-  "https://www.senscritique.com/ryoh/collection?page=" ^ (string_of_int page) ^ "&universe=" ^ (universe_tag uni) ^ "&action=RATING"
+  "https://www.senscritique.com/" ^ username ^ "/collection?page=" ^ (string_of_int page) ^ "&universe=" ^ (universe_tag uni) ^ "&action=RATING"
 
 let sc_endpoint_uri page =
   sc_endpoint page MOVIES |> Uri.of_string
@@ -28,16 +32,25 @@ let extract_collection_data body =
     let pattern = {|<script id="__NEXT_DATA__" type="application/json">\(.*\)</script><script src=|} in
     let rgx = Str.regexp pattern in
     match Str.search_forward rgx body 0 with
-    | _i -> Some (Str.matched_group 1 body)
     | exception Not_found -> None
+    | _ -> Some (Str.matched_group 1 body)
 
 let fetch_sc_collection_page page =
   sc_endpoint_uri page |> Client.get >>= fun (_, body) ->
     body |> Body.to_string >|= fun body -> body
 
+let parse_json json =
+  let json = List.fold_left (fun json member_name -> YojsonBU.member member_name json) json json_members in
+  YojsonBU.keys json |> List.iter print_endline;
+  ()
+
 let () =
   let body = Lwt_main.run (fetch_sc_collection_page 1) in
   let data = extract_collection_data body in
-  match data with
-  | Some data -> print_endline data
-  | None -> print_endline "No data found"
+  let json = match data with
+    | Some s -> Some (YojsonB.from_string s)
+    | None -> None
+  in
+  match json with
+  | Some json -> parse_json json
+  | None -> print_endline "Fetching failed"
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Senscritique to evil (letterboxd)
+
+## Idea
+
+Automatically update evil watched films and ratings from a Sencritique account
+
+## Status
+
+Project is dead for now since Letterboxd api is closed
